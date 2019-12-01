0adf927848ce48509dcfcb7718d2261b6e1dfe83
Derek Lao <dlao00@stuy.edu>
2019-12-01 12:49:53 -0500

    fixed problem where all the values in the argument are the same.

--- function.c
+++ function.c
@@ -2,24 +2,19 @@
 
 char ** parse_args( char * line )//from what I read online, standards conventions is that the args ends with a null argument
 {
-  char * arg = malloc(100 * sizeof(char *));
+  // char * arg = malloc(100 * sizeof(char *));
   int argIndex;
   char ** argArray = malloc(5 * sizeof(char **));//memory is allocated
   for(argIndex = 0 ; argIndex < 5 ; argIndex ++)
   {
     if(!line)
     {
-      // printf("final stretch now!\n");
       argIndex = 5;
     }
     else
     {
-      strcpy(arg, strsep(&line, " ")); // line is changed to the next argument
-      argArray[argIndex] = arg;
-      printf("arg is %s\n", arg);
-      printf("argArray[argIndex] is %s\n", argArray[argIndex]);
+      argArray[argIndex] = strsep(&line, " ");
     }
   }
-  printf("argArray[0] is %s\n", argArray[0]);
   return argArray;
 }
