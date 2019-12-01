15ee1d8c576972d551205de7a39934a7ca5a2880
Derek Lao <dlao00@stuy.edu>
2019-12-01 04:16:41 -0500

    made lots of progres, not done

--- main.c
+++ main.c
@@ -3,11 +3,14 @@
 int main()
 {
   printf("running...\n");
-  char * line1 = "ls -a -l\0";
-  printf("hello!\n");
-  char ** args= parse_args(line1);//memory already allocated
+  char line1[100] = "ls -a -l\0";
+  char ** args = parse_args(line1);//memory already allocated
   printf("right before execvp runs...\n");
   execvp(args[0], args);
   printf("shoudln't have gotten here...\n");
+  // char line[100] = "woah-this-is-cool";
+  // char *s1 = line;
+  // printf("[%s]\n", strsep( &s1, "-" ));
+  // printf("[%s]\n", s1);
   return 0;
 }
